8e473a33e26d7fea7ec88048fe1590d133af7a1b
Pedro Escoboza <psebastian01@hotmail.com>
2023-04-20 15:32:14 -0600

    :sparkles: Random cats

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,21 @@ const CATS: { [id: string]: { id: string; name: string; image: string } } = {
         name: 'Ben',
         image: 'https://images.pexels.com/photos/2539094/pexels-photo-2539094.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     },
+    '4': {
+        id: '4',
+        name: 'Ginger',
+        image: 'https://images.pexels.com/photos/1170986/pexels-photo-1170986.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    },
+    '5': {
+        id: '5',
+        name: 'Chilli',
+        image: 'https://images.pexels.com/photos/1317844/pexels-photo-1317844.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    },
+    '6': {
+        id: '6',
+        name: 'Wiwi',
+        image: 'https://images.pexels.com/photos/3318215/pexels-photo-3318215.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    },
 };
 
 app.get('/health', (req, res) => {
@@ -30,13 +45,16 @@ const Cat = (props: typeof CATS[keyof typeof CATS]) => {
     <li>
         <h3>${props.name}<h3>
         <img width="300px" height="300px" src="${props.image}"/>
-        <a href="/cats/${props.id}" target="_blank">Go to API URL</a>
+        <a href="/api/v1/cats/${props.id}" target="_blank">Go to API URL</a>
     </li>`;
 };
 
 app.get('/', (req, res) => {
     res.send(`
     <h1>Cats</h1>
+    <a href="/random-cat">
+        <button>Random cat</button>
+    </a>
     <ul>
         ${Object.values(CATS)
             .map((cat) => Cat(cat))
@@ -44,11 +62,32 @@ app.get('/', (req, res) => {
     </ul>
     `);
 });
-app.get('/cats', (req, res) => {
+
+function randomChoice<T>(arr: T[]): T {
+    const index = Math.floor(Math.random() * arr.length);
+    return arr[index];
+}
+
+app.get('/random-cat', (req, res) => {
+    const cat = randomChoice(Object.values(CATS));
+
+    res.send(`
+        <h1>Random cat</h1>
+        ${Cat(cat)}
+        <a href="/random-cat">
+            <button>Another random cat</button>
+        </a>
+        <a href="/">
+            <button>All cats</button>
+        </a>
+    `);
+});
+
+app.get('/api/v1/cats', (req, res) => {
     res.json(CATS);
 });
 
-app.get('/cats/:id', (req, res) => {
+app.get('/api/v1/cats/:id', (req, res) => {
     const cat = CATS[req.params.id];
     if (cat != null) return res.json(cat);
     res.status(404).send('Not Found');
